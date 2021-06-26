30da483bee53e12d0f716467439741aaba7c50a4
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-06-27 01:04:41 +0430

    flutter_white_ui

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_white_ui/screen/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
